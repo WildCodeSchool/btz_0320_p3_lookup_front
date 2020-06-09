5749b06cf630c8affda6625a85361522e1faeedc
Neo-Ryo <marco.sch4064@gmail.com>
2020-06-09 10:02:01 +0200

    created basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Hello React World</header>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CarouselHome from './CarouselHome';
+
+const Home = () => {
+  return (
+    <div>
+      <p>ok</p>
+      <CarouselHome />
+    </div>
+  );
+};
+
+export default Home;
