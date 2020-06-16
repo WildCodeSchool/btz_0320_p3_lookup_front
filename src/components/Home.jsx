949e4007b63156507751ac009e441b1e9b0caf5f
Marion-H <doudou6500@gmail.com>
2020-06-16 10:28:57 +0200

    add a component PressRelation and add in component Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CarouselHome from './carouselhome/CarouselHome';
 import Catalogue from './catalogue/Catalogue';
+import PressRelation from './PressRelation';
 
 const Home = () => {
   return (
     <div>
       <CarouselHome />
       <Catalogue />
+      <PressRelation />
     </div>
   );
 };
---
+++ src/components/PressRelation.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PressRelation() {
+  return (
+    <div>
+      <h1>Relation presse</h1>
+    </div>
+  );
+}
