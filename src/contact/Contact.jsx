d27f2318fbd042ea11fbd0f1207d03f2ba5e0fee
Marion-H <doudou6500@gmail.com>
2020-06-17 19:02:19 +0200

    add modification on css for size text and margin

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -14,79 +14,78 @@ import styles from './contact.module.css';
 export default function Contact() {
   return (
     <Container fluid className={styles.div}>
-      <Row>
+      <Row className="d-flex align-items-center justify-content-center">
+        <Col className={styles.col}>
+          <h1 className={styles.h1}>Vous souhaitez nous contactez ?</h1>
+          <p className={styles.h3}>Société Look up</p>
+          <p className={styles.p}>lookup-france@protonmail.com</p>
+          <p className={styles.p}>+33 (0) 6 88 61 93 53</p>
+        </Col>
         <Col>
-          <Container>
+          <Form className="mr-3">
             <Row>
-              <Col className={styles.col}>
-                <h1>Vous souhaitez nous contactez ?</h1>
-                <h3>Société Look up</h3>
-                <h3>lookup-france@protonmail.com</h3>
-                <h3>+33 (0) 6 88 61 93 53</h3>
+              <Col sm={6}>
+                <Input
+                  className={styles.input}
+                  type="lastname"
+                  name="lastname"
+                  id="lastname"
+                  placeholder="Nom"
+                  required
+                />
               </Col>
-              <Col className={styles.col}>
-                <Form>
-                  <Row>
-                    <Col sm={6}>
-                      <Input
-                        type="lastname"
-                        name="lastname"
-                        id="lastname"
-                        placeholder="Nom"
-                        required
-                      />
-                    </Col>
-                    <Col sm={6}>
-                      <Input
-                        type="firstname"
-                        name="firstname"
-                        id="firstname"
-                        placeholder="Prénom"
-                        required
-                      />
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col sm={6}>
-                      <Input
-                        type="tel"
-                        name="phone"
-                        id="phone"
-                        placeholder="Numéro de téléphone"
-                        pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}"
-                        required
-                      />
-                    </Col>
-                    <Col sm={6}>
-                      <Input
-                        type="adress"
-                        name="adress"
-                        id="adress"
-                        placeholder="Adresse"
-                      />
-                    </Col>
-                  </Row>
-                  <FormGroup check row>
-                    <Col sm={12}>
-                      <Input
-                        type="textarea"
-                        name="message"
-                        id="message"
-                        placeholder="Votre message"
-                        required
-                      />
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup check row>
-                    <Col sm={12}>
-                      <Button>Submit</Button>
-                    </Col>
-                  </FormGroup>
-                </Form>
+              <Col sm={6}>
+                <Input
+                  className={styles.input}
+                  type="firstname"
+                  name="firstname"
+                  id="firstname"
+                  placeholder="Prénom"
+                  required
+                />
               </Col>
             </Row>
-          </Container>
+            <Row>
+              <Col sm={6}>
+                <Input
+                  className={styles.input}
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  placeholder="Numéro de téléphone"
+                  pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}"
+                  required
+                />
+              </Col>
+              <Col sm={6}>
+                <Input
+                  className={styles.input}
+                  type="adress"
+                  name="adress"
+                  id="adress"
+                  placeholder="Adresse"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={12}>
+                <Input
+                  className={styles.input}
+                  type="textarea"
+                  name="message"
+                  id="message"
+                  placeholder="Votre message"
+                  required
+                />
+              </Col>
+            </Row>
+
+            <FormGroup check row>
+              <Col sm={12} className={styles.input}>
+                <Button>Submit</Button>
+              </Col>
+            </FormGroup>
+          </Form>
         </Col>
       </Row>
     </Container>
