b0ffbc1f6e373ccaba3b65eaccf89a59af0e89dd
Neo-Ryo <marco.sch4064@gmail.com>
2020-06-17 14:25:48 +0200

    add company to modal

--- src/components/Produit/ImageProduit.jsx
+++ src/components/Produit/ImageProduit.jsx
@@ -36,7 +36,7 @@ function ImageProduit({ buttonLabel }) {
           {buttonLabel}
         </Button>
         <Modal isOpen={modal} toggle={toggle} className="modal-lg">
-          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+          <ModalHeader toggle={toggle}>DEVIS</ModalHeader>
           <ModalBody>
             <div>
               <img
@@ -67,7 +67,16 @@ function ImageProduit({ buttonLabel }) {
             <Form>
               <FormGroup>
                 <Label for="name">Nom</Label>
-                <Input type="text" name="nom" id="name" placeholder="nom" />
+                <Input type="text" name="nom" id="name" placeholder="Mme/Mr" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="company">Entreprise</Label>
+                <Input
+                  type="text"
+                  name="company"
+                  id="company"
+                  placeholder="entreprise"
+                />
               </FormGroup>
               <FormGroup>
                 <Label for="Email">Email</Label>
