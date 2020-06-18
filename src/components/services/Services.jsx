e0ff38842160194436e930c7bfa80a7d26ff4bbc
Neo-Ryo <marco.sch4064@gmail.com>
2020-06-18 11:06:26 +0200

    solved css issues

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -8,39 +8,34 @@ const conference = require('./conference.png');
 
 export default function Services() {
   return (
-    <div>
-      <Row>
-        <Col lg="6">
-          <h1 className={styles.title1}>
-            {' '}
-            Ateliers & <br /> Conférences
-          </h1>
-          <h3 className={styles.title2}>
-            "L'impact des nouvelles technologies sur nos vies"
-          </h3>
-          <p className={styles.text}>
-            Avec LookUp nous souhaitons aller plus loin dans ce que l'on
-            pourrait appeler le digital en pleine conscience. <br />{' '}
-          </p>
-
-          <p className={styles.text}>
-            Nous proposons des interventions auprès des écoles, universités et
-            entreprises pour expliquer le fonctionnement des nouvelles
-            technologies et des réseaux sociaux pour mieux les appréhender et
-            revoir la place qu'ils occupent dans notre quotidien.{' '}
-          </p>
-          <div>
-            <Button className={styles.contact}>Contactez-nous</Button>
-          </div>
-        </Col>
-        <Col lg="6">
-          <img
-            className={styles.conference}
-            src={conference}
-            alt="présentation"
-          />
-        </Col>
-      </Row>
-    </div>
+    <Row>
+      <Col lg="6">
+        <h1 className={styles.title1}>
+          {' '}
+          Ateliers & <br /> Conférences
+        </h1>
+        <h3 className={styles.title2}>
+          "L'impact des nouvelles technologies sur nos vies"
+        </h3>
+        <p className={styles.text}>
+          Avec LookUp nous souhaitons aller plus loin dans ce que l'on pourrait
+          appeler le digital en pleine conscience. <br />{' '}
+        </p>
+        <p className={styles.text}>
+          Nous proposons des interventions auprès des écoles, universités et
+          entreprises pour expliquer le fonctionnement des nouvelles
+          technologies et des réseaux sociaux pour mieux les appréhender et
+          revoir la place qu'ils occupent dans notre quotidien.{' '}
+        </p>
+        <Button className={styles.contact}>Contactez-nous</Button>
+      </Col>
+      <Col lg="6">
+        <img
+          className={styles.conference}
+          src={conference}
+          alt="présentation"
+        />
+      </Col>
+    </Row>
   );
 }
