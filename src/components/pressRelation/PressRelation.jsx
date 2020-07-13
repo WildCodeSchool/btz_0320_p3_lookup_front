26f73c5dea1ce2a357face9e7322003ed679c27f
Neo-Ryo <marco.sch4064@gmail.com>
2020-07-13 11:57:51 +0200

    relation presse linked with backoffice

--- src/components/pressRelation/PressRelation.jsx
+++ src/components/pressRelation/PressRelation.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import { Row, Container } from 'reactstrap';
-
+import React, { useState, useEffect } from 'react';
+import { Row, Container, Spinner } from 'reactstrap';
+import axios from 'axios';
 import Press from './Press';
 
-const press = [
-  {
-    image:
-      'https://upload.wikimedia.org/wikipedia/fr/thumb/9/98/France_Bleu_logo_2015.svg/1200px-France_Bleu_logo_2015.svg.png',
-    texte: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-  },
-  {
-    image:
-      'https://www.iae-france.fr/wp-content/uploads/2018/12/20181206-LEtudiant.png',
-    texte: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-  },
-  {
-    image:
-      'https://www.offremedia.com/sites/default/files/vignette/article/nl1608-logo-capital.png',
-    texte: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-  },
-  {
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Europe_1_logo_%282010%29.svg/1280px-Europe_1_logo_%282010%29.svg.png',
-    texte: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-  },
-];
-
 export default function PressRelation() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [presse, setPresse] = useState([]);
+
+  useEffect(() => {
+    const getPress = async () => {
+      try {
+        const res = await axios.get(
+          'https://btz-js-202003-p3-lookup-back.jsrover.wilders.dev/press'
+        );
+        setPresse(res.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getPress();
+  }, []);
+
+  if (isLoading) {
+    return <Spinner color="info" />;
+  }
+
   return (
     <Container fluid="lg" className="mt-5">
       <Row>
-        {press.map((relation) => {
-          return <Press image={relation.image} texte={relation.texte} />;
+        {presse.map((relation) => {
+          return (
+            <Press image={relation.picture} texte={relation.description} />
+          );
         })}
       </Row>
     </Container>
